423957c5cdef2c44a3ba95da044316ae223d274c
root <root@centos7.localdomain>
2019-08-14 17:58:00 +0900

    AllFree

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  default = "us-east1"
+}
+
+variable "region_zone" {
+  default = "us-east1-b"
+}
+
+variable "test_ssh_keys" {
+  type    = "string"
+  default = <<EOF
+testuser:ssh-rsa xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 
+EOF
+}
